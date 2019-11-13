9a5c4d7adde7827d1e4eff2743b4047beae0acb3
jordi.pons <jordi.pons@typeform.com>
2019-11-13 12:50:59 +0100

    Adds cke editor

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Editor from './editor'
 import SlateEditor from './slate-editor'
-import DraftEditor from './draft-editor'
-import Trix from './trix'
-import QuillEditor from './quill-editor'
 import MediumEditor from './medium-editor'
 import TinyMceEditor from './tinymce-editor/tinymce-editor';
+import CKEEditor from './cke-editor/cke-editor';
 
 function App() {
   return (
     <div className="App App-header">
       <SlateEditor />
       <hr />
-      <DraftEditor />
-      <hr />
-      {/* <Editor />
-      <br /> */}
-      <QuillEditor />
-      <hr />
-      <Trix />
-      <hr />
       <MediumEditor />
       <hr />
       <TinyMceEditor />
+      <hr />
+      <CKEEditor />
     </div>
   );
 }
---
+++ src/cke-editor/cke-editor.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import CKEditor from '@ckeditor/ckeditor5-react';
+import BalloonEditor from '@ckeditor/ckeditor5-build-balloon'
+
+
+class CKEEditor extends React.Component {
+  render() {
+    return (
+      <div>
+        <span>CKEEditor</span>
+        <a href='https://ckeditor.com/docs/ckeditor5/latest/builds/'>https://ckeditor.com/ckeditor-5//</a>
+        <CKEditor
+          editor={ BalloonEditor }
+          data="Hello from CKEditor 5!"
+          onInit={ editor => {
+              // You can store the "editor" and use when it is needed.
+              console.log( 'Editor is ready to use!', editor );
+          } }
+          onChange={ ( event, editor ) => {
+              const data = editor.getData();
+              console.log( { event, editor, data } );
+          } }
+      />
+      </div>
+    )
+  }
+}
+
+export default CKEEditor
